6f3cd630c9ba0d78afa18c29ff1d8912ee662f52
yuxianye <yuxianye@163.com>
2024-03-05 15:47:52 +0800

    更新菜单

--- sql/biz/10_stationMenu.sql
+++ sql/biz/10_stationMenu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('工位', '2030', '1', 'station', 'business/station/index', 1, 0, 'C', '0', '0', 'business:station:list', '#', 'admin', sysdate(), '', null, '工位菜单');
+values('工位', '2030', '1', 'station', 'business/station/index', 1, 0, 'C', '0', '0', 'business:station:list', 'nested', 'admin', sysdate(), '', null, '工位菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
--- sql/biz/11_equipmentMenu.sql
+++ sql/biz/11_equipmentMenu.sql
@@ -1,6 +1,6 @@
 -- 菜单 SQL
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
-values('设备管理', '2030', '1', 'equipment', 'business/equipment/index', 1, 0, 'C', '0', '0', 'business:equipment:list', '#', 'admin', sysdate(), '', null, '设备菜单');
+values('设备管理', '2030', '1', 'equipment', 'business/equipment/index', 1, 0, 'C', '0', '0', 'business:equipment:list', 'nested', 'admin', sysdate(), '', null, '设备菜单');
 
 -- 按钮父菜单ID
 SELECT @parentId := LAST_INSERT_ID();
